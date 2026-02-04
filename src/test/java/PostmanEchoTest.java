5dc0e90dbeb9863589d8819d708af290fed95335
Ksenia Krestsova <arhaatuana@gmail.com>
2026-02-05 00:36:49 +0200

    initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,27 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoTest {
+
+    @Test
+
+    public void shouldPostCorrectly() {
+
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("hello, Postman") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("hello, Postman"))
+        ;
+    }
+}
